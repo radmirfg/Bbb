e5dcd5ff57bd6dbf21474208d251976ff6f9bcde
radmirfg <138251877+radmirfg@users.noreply.github.com>
2025-05-26 17:48:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,39 @@
-TOKEN = "111" # Токен бота
-CRYPTO = "111" # Токен CryptoBot
-ADMINS = [111, 111, 111, 111] # ID Админов
+TOKEN = "AAH9EKu_uotJ1mQdkl4FVaf6xSak7RBpRIE" # Токен бота
+
+CRYPTO = "AA04cjxIyGzouh3K53CtmtJhpqmFvvUugF2" # Токен CryptoBot
+
+ADMINS = [6141927434,1324851043] # ID Админов
+
+
 
 # Bot Options
-bot_name = "@"
-bot_logs = "ZZZZZ"
+
+bot_name = "@amamvplol_bot"
+
+bot_logs = "@Atlant_https"
+
 bot_channel_link = "https://t.me/test"
-bot_admin = "VVVVVV"
+
+bot_admin = "@Atlant_https"
+
 bot_documentation = "https://t.me/test"
+
 bot_works = "https://t.me/test"
+
 bot_reviews = "https://t.me/test"
 
+
+
 # Price
+
 subscribe_1_day = 0.99
+
 subscribe_7_days = 2.99
+
 subscribe_14_days = 3.99
+
 subscribe_30_days = 4.99
+
 subscribe_365_days = 6.99
+
 subscribe_infinity_days = 7.99
